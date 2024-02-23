9e5cd40a9d88a2a163c68d3ed94bed7cbe9515cf
shisha-cpu <79801541+shisha-cpu@users.noreply.github.com>
2024-02-23 22:11:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,26 +13,6 @@
 <?php
     require('./blocks/header.php')
     ?>
-
-    <div class="container">
-        <h3>Наши статьи</h3>
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-            <?php for ($i = 0; $i < 5; $i++): ?>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/<?php echo($i + 1) ?>.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Название статьи</h5>
-                        <p class="card-text">Краткое описание статьи.</p>
-                        <a href="#" class="btn btn-primary">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <?php endfor; ?>
-        </div>
-    </div>
-    <?php
-    require('./blocks/footer.php')
-    ?>
+SALALALALALA
 </body>
 </html>
